49231b83d71b1e567a5b33bddc0fd8200901cbe4
Sneezry <lizhe@lizhe.org>
2014-11-21 19:12:25 +0800

    1

--- controllers.js
+++ controllers.js
@@ -1,13 +1,13 @@
 var DD = DD || angular.module('DD',['ngRoute']);
 
-DD.controller('AuthController', function($scrop){
+DD.controller('AuthController', function($scrope){
 	$scrop.auth = function(){
 		var authUrl = g.getAuthURL('37166cf1a32a1b3f6701',['user','repo']);
 		$location(authUrl);
 	}
 });
 
-DD.controller('HomeController', function($scrop){
+DD.controller('HomeController', function($scrope){
 	g.getRepoList(function(repos){
 		$scrop.repos = repos;
 	});
