f64f92ef0178dcb8939150c8c3107a156050b7f9
Sneezry <lizhe@lizhe.org>
2014-11-21 17:12:36 +0800

    1

---
+++ github.api.js
@@ -0,0 +1,52 @@
+var github = function(token){
+	var scope = this;
+
+	scope.githubApiRequest = function(uri, method, parameter, callback){
+		if(!method){
+			method = 'GET';
+			parameter = method;
+			callback = parameter;
+		}
+		if(typeof(parameter)=='function'){
+			callback = parameter;
+			parameter = {};
+		}
+		else{
+			parameter = parameter?parameter:{};
+		}
+		parameter.access_token = token;
+
+		var url = 'https://api.github.com'+uri;
+		$.ajax({
+			url: url,
+			type: method,
+			data: parameter,
+			dataType: 'json',
+			success: callback
+		});
+	}
+
+	scope.getUserInfo = function(callback){
+		scope.githubApiRequest('/user', callback);
+	};
+
+	scope.getRepoList = function(callback){
+		scope.githubApiRequest('/user/repos', callback);
+	};
+
+	scope.createRepo = function(options, callback){
+		scope.githubApiRequest('/user/repos', 'POST', options, callback);
+	};
+
+	scope.getContent = function(owner, repo, path, callback){
+		scope.githubApiRequest('/repos/'+owner+'/'+repo+'/contents/'+path, callback);
+	};
+
+	scope.createFile = function(owner, repo, path, callback){
+		scope.githubApiRequest('/repos/'+owner+'/'+repo+'/contents/'+path, 'PUT', callback);
+	};
+
+	scope.deleteFile = function(owner, repo, path, callback){
+		scope.githubApiRequest('/repos/'+owner+'/'+repo+'/contents/'+path, 'DELETE', callback);
+	};
+}
