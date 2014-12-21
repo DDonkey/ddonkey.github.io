b6e8f9b129d6ce4aa1184e44a707154ffe963725
rebornix <penn.lv@gmail.com>
2014-12-21 21:10:32 +0800

    blog repo analysis

--- app/scripts/controllers/main-controller.js
+++ app/scripts/controllers/main-controller.js
@@ -8,17 +8,23 @@
  * Controller of the ddonkeyApp
  */
 angular.module('ddonkeyApp')
-  .controller('MainCtrl', ['$scope', 'github', 'topicModel', '$state', '$rootScope', '$location',
-    function ($scope, github, topicModel, $state, $rootScope, $location) {
-        console.log($location.host());
+  .controller('MainCtrl', ['$scope', 'github', 'topicModel', '$state', '$rootScope',
+    function ($scope, github, topicModel, $state, $rootScope) {
+    $scope.rootScope = $rootScope;
     github.getRepoList().success(function(repos){
         if ($rootScope.currentUser) {
+            console.log($rootScope.currentUser);
             $scope.blogRepo = _.find(repos, {name: $rootScope.currentUser.login.toLowerCase() + ".github.io"});
+            $scope.fetchChildren($scope.blogRepo.full_name);
+            $scope.fetchCommitActivity($scope.blogRepo.full_name);
         }
         else {
             github.getUserInfo().success(function(info){
                 $rootScope.currentUser = info;
+                console.log(info);
                 $scope.blogRepo = _.find(repos, {name: $rootScope.currentUser.login.toLowerCase() + ".github.io"});
+                $scope.fetchChildren($scope.blogRepo.full_name);
+                $scope.fetchCommitActivity($scope.blogRepo.full_name);
               });
         }
     });
@@ -29,6 +35,35 @@ angular.module('ddonkeyApp')
     }
 
     $scope.blogList = [];
+
+    $scope.fetchCommitActivity = function(id) {
+        var owner = id.split('/')[0];
+        var repo  =id.split('/')[1];
+        github.getRepoCommitActivity(owner, repo).then(function(resp) {
+            var ctx = document.getElementById("commitActChart").getContext("2d");
+            var options = {};
+            var labels = [];
+            var data = [];
+            for(var i = 1; i <= 52; i++) {
+                labels.push(i);
+                data.push(resp.data[i - 1].total);
+            }
+            var data = {
+                labels: labels,
+                datasets: [
+                    {
+                        label: "My First dataset",
+                        fillColor: "rgba(220,220,220,0.5)",
+                        strokeColor: "rgba(220,220,220,0.8)",
+                        highlightFill: "rgba(220,220,220,0.75)",
+                        highlightStroke: "rgba(220,220,220,1)",
+                        data: data
+                    }
+                ]
+                };
+            var myBarChart = new Chart(ctx).Bar(data, options);
+        });
+    };
 
     $scope.fetchChildren = function(id) {
     	var owner = id.split('/')[0];
